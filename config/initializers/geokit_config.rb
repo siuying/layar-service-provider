29fea9d6781297a7466ba781e28f516a600d6f5d
siuying <siu.ying@gmail.com>
2009-09-12 23:42:10 +0800

    use environment var for gmap key

--- config/initializers/geokit_config.rb
+++ config/initializers/geokit_config.rb
@@ -26,7 +26,7 @@ if defined? Geokit
 	# This is your Google Maps geocoder key. 
 	# See http://www.google.com/apis/maps/signup.html
 	# and http://www.google.com/apis/maps/documentation/#Geocoding_Examples
-	Geokit::Geocoders::google = 'ABQIAAAAHkXTEjC7ErFpCj-xqqWd0hRxiwgtZmcKZZrn-dAmkO3QPxlMoBSMLmoE9KonndGc98YVFV3hqYlnvg'
+	Geokit::Geocoders::google = ENV['GMAP_KEY']
     
 	# This is your username and password for geocoder.us.
 	# To use the free service, the value can be set to nil or false.  For 
