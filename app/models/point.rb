5da1842f3e755eb04f2cd7123327d605021240ba
siuying <siu.ying@gmail.com>
2009-09-12 23:54:36 +0800

    remove deprecated function

--- app/models/point.rb
+++ app/models/point.rb
@@ -5,21 +5,5 @@ class Point < ActiveRecord::Base
                    :distance_field_name => :distance,
                    :lat_column_name => :lat,
                    :lng_column_name => :lng
-                   
-  def to_poi(current_point)
-    {
-       :actions => [], 
-       :distance => distance_to(current_point) * 1000,
-       :attribution => "data from www.mcdonalds.com.hk",
-       :id => "#{name}#{id}",
-       :imageURL => nil,
-       :lat => (lat * 1000000).to_i,
-       :lon => (lng * 1000000).to_i,
-       :title => name,
-       :line2 => address,
-       :line3 => phone,
-       :line4 => "",
-       :type => 0
-    }
-  end
+
 end
